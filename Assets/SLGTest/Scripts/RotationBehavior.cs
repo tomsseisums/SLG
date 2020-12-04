7fc1d0dc065b25cb88737c928997b2a0644c67a3
Toms Seisums <toms.seisums@gmail.com>
2020-12-04 18:31:44 +0200

    Added RotationBehavior
    - Renamed SampleScene to DefaultScene
    - Normalized lighting settings

---
+++ Assets/SLGTest/Scripts/RotationBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private float degreesPerSecond = 360f;
+
+    [SerializeField]
+    private Vector3 rotationAxis = Vector3.up;
+
+    void Update()
+    {
+        transform.Rotate(rotationAxis * degreesPerSecond * Time.deltaTime);
+    }
+}
