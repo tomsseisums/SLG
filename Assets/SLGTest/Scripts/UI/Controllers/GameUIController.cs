de2ec35a367c0fe321fc5c547c784c0e80a5ce76
Toms Seisums <toms@nethart.com>
2020-12-05 14:55:48 +0200

    Added Game Scene
    - Added UI Controllers

---
+++ Assets/SLGTest/Scripts/UI/Controllers/GameUIController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameUIController : MonoBehaviour
+{
+    [SerializeField]
+    private Button quitButton;
+
+    [SerializeField]
+    private string menuSceneName;
+
+    private void OnEnable()
+    {
+        quitButton.onClick.AddListener(QuitGame);
+    }
+
+    private void OnDisable()
+    {
+        quitButton.onClick.RemoveListener(QuitGame);
+    }
+
+    private void QuitGame()
+    {
+        SceneManager.LoadScene(menuSceneName);
+    }
+}
---
+++ Assets/SLGTest/Scripts/UI/Controllers/MenuUIController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuUIController : MonoBehaviour
+{
+    [SerializeField]
+    private Button startButton;
+
+    [SerializeField]
+    private string gameSceneName;
+
+    private void OnEnable()
+    {
+        startButton.onClick.AddListener(LoadGame);
+    }
+
+    private void OnDisable()
+    {
+        startButton.onClick.RemoveListener(LoadGame);
+    }
+
+    private void LoadGame()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+}
